ac06e40c878b363e70a8942a8cc7cbd5c1172196
yaroslaDartc <yaroslav@dartc.ua>
2018-02-07 13:16:33 +0200

    add delete old files

--- CustomLog.php
+++ CustomLog.php
@@ -2,19 +2,50 @@
 
 class CustomLog
 {
+    /**
+     * Папка для логов
+     */
     const LOG_FOLDER = '/log/';
+    /**
+     * Количество файлов, которое будет хранится в папке с логом
+     */
+    const MAX_COUNT_LOG_FILE = 40;
 
+    /**
+     * Имя файла текущего лога
+     * @var string
+     */
     private $file;
 
-    public function __construct($fileName) {
+    /**
+     * CustomLog constructor.
+     * @param string $fileName
+     * @param bool $clearOldFile
+     */
+    public function __construct($fileName, $clearOldFile = true) {
         $dir = __DIR__ . self::LOG_FOLDER;
+
         if (!file_exists($dir)) {
             mkdir($dir, 0777);
+        } elseif ($clearOldFile) {
+            $masFile = glob($dir . $fileName . "_*");
+            if (count($masFile) > self::MAX_COUNT_LOG_FILE) {
+                rsort($masFile);
+
+                for ($i = self::MAX_COUNT_LOG_FILE; $i < count($masFile); $i++) {
+                    unlink($masFile[$i]);
+                }
+
+            }
         }
 
         $this->file = $dir . $fileName . '_' . date('Y-m-d') . '.log';
     }
 
+    /**
+     * Логирует данные
+     * @param $data
+     */
     public function log($data) {
         $str = print_r($data, true);
         if ($str[strlen($str) - 1] != "\n") {
